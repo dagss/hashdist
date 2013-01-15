8310d5d0e9d6497d424e67bcdbdcead8e2abd5b1
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2013-01-15 16:37:26 +0100

    hdist use: basic querying of attrs working; next step: schema

---
+++ hashdist/cli/use_cli.py
@@ -0,0 +1,33 @@
+import os
+from os.path import join as pjoin
+
+from .main import register_subcommand
+
+@register_subcommand
+class Use(object):
+    """
+    Prints instructions to set up profile in the current shell
+
+    The profile will be built if it is not available.
+
+    The normal use of this command is to source it into the current
+    shell, e.g.::
+
+        source <(hdist env path/to/profile/spec myprofile)
+    """
+
+    command = 'use'
+
+    @staticmethod
+    def setup(ap):
+        ap.add_argument('path', help='path to YAML profile specification')
+        ap.add_argument('profile', nargs='?', help='name of profile (default: "default")')
+
+    @staticmethod
+    def run(ctx, args):
+        from ..frontend import build_yaml_profile
+        if not args.profile:
+            args.profile = 'default'
+        build_yaml_profile(args.path, args.profile, {})
+
+
