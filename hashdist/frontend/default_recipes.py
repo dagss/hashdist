2b4b29b43c7d169abe2244f872cfecc33a1af737
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2013-01-16 15:01:32 +0100

    split configuration and assemble(-build-spec) pipelines

--- hashdist/frontend/default_recipes.py
+++ hashdist/frontend/default_recipes.py
@@ -2,7 +2,7 @@ from ..deps.yaml import safe_load as yaml
 
 ncores = 4
 
-def configure_make_install(ctx, cfg, attrs, build_spec):
+def configure_make_install(ctx, attrs, build_spec):
     if attrs['recipe'] != 'configure-make-install':
         return
 
@@ -20,7 +20,7 @@ def configure_make_install(ctx, cfg, attrs, build_spec):
     build_spec['build']['env_nohash']['NCORES'] = str(ncores)
 
 
-def symlink_everything(ctx, cfg, attrs, build_spec):
+def symlink_everything(ctx, attrs, build_spec):
     # todo: when should this not happen?
     artifact_spec_file = yaml('''
     target: $ARTIFACT/artifact.json
@@ -37,23 +37,23 @@ def symlink_everything(ctx, cfg, attrs, build_spec):
 
     build_spec['files'].append(artifact_spec_file)
 
-def specify_sources(ctx, cfg, attrs, build_spec):
+def specify_sources(ctx, attrs, build_spec):
     sources = attrs.get('sources', None)
     if sources:
         strip = 1 if sources.startswith('tar') else 0
         build_spec['sources'].append(dict(strip=strip, target='.', key=sources))
 
-def write_files(ctx, cfg, attrs, build_spec):
+def write_files(ctx, attrs, build_spec):
     if build_spec['files']:
         build_spec['build']['script'].insert(0, ['@hdist', 'build-write-files'])
 
-def unpack_sources(ctx, cfg, attrs, build_spec):
+def unpack_sources(ctx, attrs, build_spec):
     if build_spec['sources']:
         build_spec['build']['script'].insert(0, ['@hdist', 'build-unpack-sources'])
 
 def register_recipes(pipeline):
     pipeline.add_recipe(configure_make_install)
-    pipeline.add_stage(after='recipes_profile_install', func=symlink_everything)
-    pipeline.add_stage(after='configuration_end', func=specify_sources)
-    pipeline.add_stage(after='post_recipes', func=write_files)
-    pipeline.add_stage(after='post_recipes', func=unpack_sources)
+    pipeline.add_assemble_stage(after='assemble_start', func=specify_sources)
+    pipeline.add_assemble_stage(after='recipes_profile_install', func=symlink_everything)
+    pipeline.add_assemble_stage(before='assemble_end', func=write_files)
+    pipeline.add_assemble_stage(before='assemble_end', func=unpack_sources)
