b0d5cfab9677242943ec60f6b36a01276f585a93
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2013-01-16 15:01:32 +0100

    split configuration and assemble(-build-spec) pipelines

--- hashdist/frontend/build.py
+++ hashdist/frontend/build.py
@@ -6,47 +6,64 @@ from ..core.utils import substitute
 
 from pprint import pprint
 
+def search_phase(pipeline, needle):
+    for i, (name, _) in enumerate(pipeline):
+        if name == needle:
+            return i
+    raise ValueError('%s not in pipeline list' % needle)
+
+def add_pipeline_stage(pipeline, after=None, func=None, before=None, name=None):
+    if int(after is None) + int(before is None) != 1:
+        raise TypeError('must specify either after or before')
+
+    if func is None:
+        def decorator(func):
+            name_ = name
+            if name_ is None:
+                name_ = func.__name__
+                add_pipeline_stage(pipeline, after, func, before, name_)
+                return func
+        return decorator
+
+    needle, offset = (before, 0) if before is not None else (after, 1)
+    i = search_phase(pipeline, needle)
+    pipeline.insert(i + offset, (name, func))
+
+def run_pipeline(pipeline, *args):
+    for name, func in pipeline:
+        if func is not None:
+            func(*args)
+    
 class StackBuildPipeline(object):
     def __init__(self):
         # [(phase_name, func)]
         # register some phase markers initially
-        self.pipeline = [
-            ('parse', None),
+        self.configuration_pipeline = [
             ('configuration_start', None),
             ('configuration_end', None),
+            ]
+        self.assemble_pipeline = [
+            ('assemble_start', None),
             ('recipes', None),
             ('recipes_profile_install', None),
             ('post_recipes', None),
-            ('build', None)]
+            ('assemble_end', None),
+            ]
 
-    def add_stage(self, after=None, before=None, name=None, func=None):
-        if int(after is None) + int(before is None) != 1:
-            raise TypeError('must specify either after or before')
+    def add_configuration_stage(self, **kw):
+        return add_pipeline_stage(self.configuration_pipeline, **kw)
 
-        if func is None:
-            def decorator(func):
-                name_ = name
-                if name_ is None:
-                    name_ = func.__name__
-                self.add_stage(after, before, name_, func)
-                return func
-            return decorator
-
-        needle, offset = (before, 0) if before is not None else (after, 1)
-        for i, (it_name, _) in enumerate(self.pipeline):
-            if it_name == needle:
-                self.pipeline.insert(i + offset, (name, func))
-                break
-        else:
-            raise ValueError("Did not find pipeline stage %s" % needle)
+    def add_assemble_stage(self, **kw):
+        return add_pipeline_stage(self.assemble_pipeline, **kw)
 
     def add_recipe(self, func, name=None):
-        self.add_stage(after='recipes', name=name, func=func)
+        return self.add_assemble_stage(after='recipes', func=func, name=name)
 
-    def run_pipeline(self, *args):
-        for name, func in self.pipeline:
-            if func is not None:
-                func(*args)
+    def run_configuration(self, *args):
+        run_pipeline(self.configuration_pipeline, *args)
+
+    def assemble_build_spec(self, *args):
+        run_pipeline(self.assemble_pipeline, *args)
 
 class StackBuildContext(object):
     def __init__(self, pipeline, logger, cache, build_store, source_cache, stack_spec):
@@ -56,11 +73,25 @@ class StackBuildContext(object):
         self.build_store = build_store
         self.source_cache = source_cache
         self.stack_spec = stack_spec
-        
-    def query_build_spec(self, package_cfg):
-        package_attrs = {}
-        package_build_spec = {}
-        pipeline.run_pipeline(ctx, package_cfg, package_attrs, package_build_spec)
+
+    def configure(self, package_cfg, package_attrs):
+        self.pipeline.run_configuration(self, package_cfg, package_attrs)
+       
+    def query_build_spec(self, package_attrs):
+        package_build_spec = {
+            "build": {
+                "script": [],
+                "import": [],
+                "env": {},
+                "env_nohash": {}},
+            "files": [],
+            "sources": [],
+            "name": package_attrs['package'],
+            "version": 'n' # for now
+            }
+
+        self.pipeline.assemble_build_spec(self, package_attrs, package_build_spec)
+        return package_build_spec
         
 
      
@@ -69,8 +100,8 @@ from .default_recipes import register_recipes
 register_recipes(pipeline)
 del register_recipes
 
-@pipeline.add_stage(after='configuration_start')
-def find_possible_versions(ctx, cfg, attrs, build_spec):
+@pipeline.add_configuration_stage(after='configuration_start')
+def find_possible_versions(ctx, cfg, attrs):
     """
     Auto-detect cfg['possible_versions'] (if neither
     'possible_versions' nor 'version' is given) by scanning the
@@ -85,16 +116,16 @@ def find_possible_versions(ctx, cfg, attrs, build_spec):
                     result.extend(c.get_mentioned_values())
         cfg['possible_versions'] = result
 
-@pipeline.add_stage(after='find_possible_versions')
-def decide_version(ctx, cfg, attrs, build_spec):
+@pipeline.add_configuration_stage(after='find_possible_versions')
+def decide_version(ctx, cfg, attrs):
     if 'version' not in cfg:
         if 'possible_versions' in cfg and len(cfg['possible_versions']) == 1:
             cfg['version'] = cfg['possible_versions'][0]
         else:
             raise ValueError('Unable to decide on version for %s' % cfg['package'])
 
-@pipeline.add_stage(after='configuration_end')
-def evaluate_build_attrs(ctx, cfg, attrs, build_spec):
+@pipeline.add_configuration_stage(before='configuration_end')
+def evaluate_build_attrs(ctx, cfg, attrs):
     add_attrs = evaluate_dict_with_conditions(ctx.stack_spec['build'], cfg)
     for key, value in add_attrs.items():
         if isinstance(value, basestring):
@@ -123,21 +154,13 @@ def build_yaml_profile(config, logger, spec_dir, profile_name, cfg):
         package_cfg.update(query)
         package_cfg = normalize_cfg_vars(package_cfg)
 
-        package_attrs = {}
-        package_build_spec = {
-            "build": {
-                "script": [],
-                "import": [],
-                "env": {},
-                "env_nohash": {}},
-            "files": [],
-            "sources": [],
-            "name": package_cfg['package'],
-            "version": 'n' # for now
-            }
-                
-        pipeline.run_pipeline(ctx, package_cfg, package_attrs, package_build_spec)
-        pprint( package_build_spec)
+        attrs = {}
+        ctx.configure(package_cfg, attrs)
+        attrs['package'] = package_cfg['package']
+        attrs['version'] = package_cfg['version']
+
+        build_spec = ctx.query_build_spec(attrs)
+        pprint( build_spec)
 
     #pprint( doc['build'])
     #pprint(profile_doc)
